4ad29f6ebbdf5dba1a4d801647afdc97de5f9530
Aztal-Samoht <aztalcreations@gmail.com>
2024-04-27 08:22:43 -0400

    remade app from scratch with new version of flutter

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# CuongNhuManualmakes
+# coung_nhu_examples
+
+a new starting point for the cuong-nhu app
--- android/app/src/main/kotlin/aztalapps/cuong_nhu_manual/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package aztalapps.cuong_nhu_manual
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
